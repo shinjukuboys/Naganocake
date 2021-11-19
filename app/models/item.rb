e9893572b2dc74b5e734f96758fe5849765f0dbb
Opens35 <ryuttti1102@gmail.com>
2021-11-19 11:22:58 +0000

    Add model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,17 @@
 class Item < ApplicationRecord
   belongs_to :genre
   attachment :image_id
+
+  validates :image, presence: true
+  validates :name, presence: true
+  validates :explanation, presence: true
+  validates :genre_id, presence: true
+  # 値段設定時、半角数字のみ登録可能にするバリデーション
+  validates :price, presence: true, format: {
+    with: /\A[0-9]+\z/i,
+  }
+
+  def self.search(word)
+    Product.where("name LIKE?", "%#{word}%")
+  end
 end
