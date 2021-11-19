8091c50adadd8943a216095029cf36321a489c4d
Opens35 <ryuttti1102@gmail.com>
2021-11-19 12:39:27 +0000

    aa

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,9 +2,9 @@ class Item < ApplicationRecord
   belongs_to :genre
   attachment :image_id
   
-  validates :genre_id, :name, :tax_out_price, presence: true
+  validates :genre_id, :name, :price, presence: true
 	validates :explanation, length: {maximum: 200}
-	validates :price, numericality: { only_integer: true }
+	validates :price, numericality: { only_integer: true}
 
   def self.search(word)
     Product.where("name LIKE?", "%#{word}%")
