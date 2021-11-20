7179dc553065958b4274c0e911568ad47e0cefd5
tayboy217 <tayboy217@icloud.com>
2021-11-20 05:48:53 +0000

    up_date

--- app/controllers/customer/registrations_controller.rb
+++ app/controllers/customer/registrations_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Customer::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
+   before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
@@ -38,7 +38,11 @@ class Customer::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # protected
+   protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email,:last_name,:first_name,:last_name_kana,:first_name_kana,:postal_code,:address,:telephone_number])
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
