24d44106611fc4b0f8369f8b5ff6ad751f90e6b7
Opens35 <ryuttti1102@gmail.com>
2021-11-20 07:24:04 +0000

    made index view

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,4 +1,4 @@
-class Adress < ApplicationRecord
+class Address < ApplicationRecord
   belongs_to :customer
   
   validates :customer_id, :name, :address, presence: true
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,5 +4,5 @@ class Customer < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          
-  has_many :address, dependent: :destroy
+  has_many :addresses, dependent: :destroy
 end
