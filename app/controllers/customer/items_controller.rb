dd3b2bb36fabcccf4b6e796e44d6ad55f0dab053
tayboy217 <tayboy217@icloud.com>
2021-11-22 06:52:40 +0000

    Update

--- app/controllers/customer/items_controller.rb
+++ app/controllers/customer/items_controller.rb
@@ -1,11 +1,12 @@
 class Customer::ItemsController < ApplicationController
   def index
     @items = Item.all
+    
   end
 
   def show
-    @items = Item.all
     @item = Item.find(params[:id])
+    @cart_item = CartItem.new
   end
 
 
