5484e10245ae46b4900e5e17ed94c3c8ae661ed3
Opens35 <ryuttti1102@gmail.com>
2021-11-24 13:27:46 +0000

    validate

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -8,4 +8,13 @@ class Customer < ApplicationRecord
   has_many :cart_items, dependent: :destroy
   
   has_many :orders, dependent: :destroy
+  
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_kana, presence: true, format: { with: /\A[\p{katakana}\u{30fc}]+\z/, message: "全角カタカナのみで入力して下さい" }
+  validates :first_name_kana, presence: true, format: { with: /\A[\p{katakana}\u{30fc}]+\z/, message: "全角カタカナのみで入力して下さい" }
+  validates :postal_cord, presence: true
+  validates :address, presence: true
+  validates :telephone_number, presence: true, numericality: {only_integer: true}
+  validates :email, presence: true, uniqueness: true
 end
