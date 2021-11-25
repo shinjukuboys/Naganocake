4b49bca98cd9cf183667efc1c4363a2ec69955d2
minamit-373 <minamit.37394@gmail.com>
2021-11-25 06:26:03 +0000

    end

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_25_061535) do
+ActiveRecord::Schema.define(version: 2021_11_25_061907) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "customer_id"
@@ -98,6 +98,7 @@ ActiveRecord::Schema.define(version: 2021_11_25_061535) do
     t.string "postal_code"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "order_status", default: 0, null: false
   end
 
 end
