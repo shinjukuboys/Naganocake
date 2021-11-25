0456e2dbf592151c33c1b10f16d3b11acb34047c
minamit-373 <minamit.37394@gmail.com>
2021-11-25 06:22:56 +0000

    removeorderstatus

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_20_065647) do
+ActiveRecord::Schema.define(version: 2021_11_25_061535) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "customer_id"
@@ -90,7 +90,6 @@ ActiveRecord::Schema.define(version: 2021_11_20_065647) do
 
   create_table "orders", force: :cascade do |t|
     t.integer "customer_id"
-    t.integer "order_status"
     t.integer "postage"
     t.integer "total_payment"
     t.integer "payment_method"
